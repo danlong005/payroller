e552938188a8e4203270d50e42330eb144e43cc2
Daniel Long <danlong5@gmail.com>
2022-07-09 19:15:55 -0500

    setup webpack for the react application

---
+++ apps/payroller-ui/client-app/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }, 
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../wwwroot'),
+  },
+  mode: process.env.PAYROLLER_ENV || 'development',
+  cache: false
+};
